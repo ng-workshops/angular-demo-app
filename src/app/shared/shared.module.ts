84adeb201d571f900269a1448cd517d2b4ca8b89
Andreas Krummsdorf <andreas.krummsdorf@doksafe.de>
2019-03-28 13:05:58 +0100

    Add prettierrc
    add updated dependencies

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,25 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule, MatRadioModule, MatSnackBarModule, MatTableModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSnackBarModule,
+  MatTableModule
+} from '@angular/material';
 import { CanClickDirective } from './directives/can-click.directive';
 import { FloatValidatorDirective } from './input-float/float.validator.directive';
 import { InputFloatComponent } from './input-float/input-float.component';
 import { JoinPipe } from './pipes/join.pipe';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    MatInputModule,
-    FormsModule
-  ],
-  declarations: [
-    CanClickDirective,
-    InputFloatComponent,
-    FloatValidatorDirective,
-    JoinPipe
-  ],
+  imports: [CommonModule, MatInputModule, FormsModule],
+  declarations: [CanClickDirective, InputFloatComponent, FloatValidatorDirective, JoinPipe],
   exports: [
     CommonModule,
     FormsModule,
@@ -38,4 +36,4 @@ import { JoinPipe } from './pipes/join.pipe';
   ],
   providers: []
 })
-export class SharedModule { }
+export class SharedModule {}
